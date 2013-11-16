fc55f1b8c8faf121427aa09c0e8d600e86043135
Maciej Benke <psqmaxor@gmail.com>
2013-11-16 15:46:40 +0100

    Notifications when page is not active, fixes #18

--- js/background.js
+++ js/background.js
@@ -7,58 +7,67 @@ var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async
 ga.src = 'https://ssl.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 
+var pageActive = false
+
 function showNotification(title, message, icon) {
-	//var notification = webkitNotifications.createHTMLNotification('notification.html?title=' + title + '&message=' + message + '&avatar=' + avatar + '&color=' + color);
-		
-	//notification.show();
-	
-	title = $("<div />").html(title).text()
-	message = $("<div />").html(message).text()
+	if(!pageActive) {
+		title = $("<div />").html(title).text()
+		message = $("<div />").html(message).text()
 
-	var options = {
-		type: 'basic',
-		title: title,
-		message: message,
-		iconUrl: "img/" + icon + ".png"
+		var options = {
+			type: 'basic',
+			title: title,
+			message: message,
+			iconUrl: "img/" + icon + ".png"
+		}
+
+		var notificationTimeout = window.localStorage["notificationTimeout"];
+		if(notificationTimeout == undefined) {
+			notificationTimeout = 5000;
+		}
+
+		chrome.notifications.create("", options, function(notificationId) {
+			setTimeout(function() {
+				chrome.notifications.clear(notificationId, function() {
+					// I DON'T CARE CHROME
+				}); 
+			}, notificationTimeout)
+		})
+
+		_gaq.push(['_trackEvent', 'LastPlug Stats', 'Notifications Served', chrome.app.getDetails().version]);
 	}
-
-	var notificationTimeout = window.localStorage["notificationTimeout"];
-	if(notificationTimeout == undefined) {
-		notificationTimeout = 5000;
-	}
-
-	chrome.notifications.create("", options, function(notificationId) {
-		setTimeout(function() {
-			chrome.notifications.clear(notificationId, function() {
-				// I DON'T CARE CHROME
-			}); 
-		}, notificationTimeout);
-	})
-
-	_gaq.push(['_trackEvent', 'LastPlug Stats', 'Notifications Served', chrome.app.getDetails().version]);
-	
 }
 
 chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if(request.method == "getLocalStorage") {
-		sendResponse({value: localStorage[request.value]});
+		sendResponse({value: localStorage[request.value]})
 	} else if(request.method == "getVersion") {
-		sendResponse({value: chrome.app.getDetails().version});
+		sendResponse({value: chrome.app.getDetails().version})
 	} else {
-		showNotification(request.title, request.message, request.icon);
+		showNotification(request.title, request.message, request.icon)
 	}
 });
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	chrome.tabs.get(activeInfo.tabId, function(tab) {
+		if(tab.url.indexOf('http://plug.dj/') > -1) {
+			pageActive = true
+		} else {
+			pageActive = false
+		}
+	})	
+})
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	if(tab.url.indexOf('plug.dj') > -1) {
-		chrome.pageAction.show(tabId);
+	console.log(tab.url)
+	if(tab.url.indexOf('http://plug.dj/') > -1) {
+		chrome.pageAction.show(tabId)
 		if(window.localStorage['enable_updates'] == "true") {
 			if(window.localStorage['lastVersion'] !== undefined) {
 				if(window.localStorage['lastVersion'] !== chrome.app.getDetails().version) {
-					showNotification('LastPlug have just been updated!', 'Updated from ' + window.localStorage['lastVersion'] + " to " + chrome.app.getDetails().version + '.', 'img/icon.png', 'red');
-					window.localStorage['lastVersion'] = chrome.app.getDetails().version;
+					showNotification('LastPlug have just been updated!', 'Updated from ' + window.localStorage['lastVersion'] + " to " + chrome.app.getDetails().version + '.', 'img/icon.png', 'red')
+					window.localStorage['lastVersion'] = chrome.app.getDetails().version
 				}
 			} else {
-				window.localStorage['lastVersion'] = chrome.app.getDetails().version;
+				window.localStorage['lastVersion'] = chrome.app.getDetails().version
 			}
 		}
 	}
